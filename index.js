08072aa90766aa13b9f86005b4050377ec40d923
Joe <hutcheson2001@yahoo.com>
2022-01-28 16:32:54 -0500

    commit tree f51e0fb937e5218377f541958dced47fea9ca163

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Add your code here
+
+function submitData(userName, userEmail){
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: userEmail
+    }) 
+  }
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then(function(response){
+      return response.json();
+    })
+    .then(function (object){
+     document.body.innerHTML = object.id 
+    })
+    .catch(function(error){
+      alert("Danger, Will Robinson!");
+      document.body.innerHTML = error.message
+    })
+}
+
+// const configurationObject = {
+//  // method: "POST", 
+//   headers: {
+//     "Content-type": "application/json",
+//     Accept: "application/jason",
+//   },
+//   body: JSON.stringify({
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+//   })
+// }
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+//   .then(function(response){
+//     return response.json();
+//   })
+//   .then(function (object){
+//     console.log(object);
+//   })
+//   .catch(function(error){
+//     alert("Bad things, bitch!");
+//     console.log(error.message);
+//   });
